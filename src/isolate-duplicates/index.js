f91a27149d3844406db222d84071a4dfccc7af69
chizoba <chizoba.victory1@gmail.com>
2022-09-13 14:36:25 +0100

    This is my solution for the  assignment

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,46 +1,46 @@
 function isolateDuplicates(text) {
-  const num = String(text).split("");
-  let incoming = text;
+  const num = String(text).split("")
+  let input = text;
   let str = "";
 
-  if (typeof text != "string") throw "Please enter a valid string";
+  if(typeof(text) != "string") throw "Please enter a valid string";
 
-  for (let i = 0; i <= incoming.length - 1; i++) {
-    if (typeof num[i] == "string") {
-      let current = String(incoming).charAt(i).toLowerCase();
-      let next = String(incoming)
-        .charAt(i + 1)
-        .toLowerCase();
-      if (current != next) {
-        str += num[i] + "<";
+  for(let i =0;i < input.length; i++){
+    if(typeof(num[i]) == "string"){
+      let current = String(input).charAt(i).toLowerCase();
+      let next = String(input)
+      .charAt(i + 1)
+      .toLowerCase();
+      if(current != next){
+        str += num[i] + "<"
       } else {
-        str += num[i];
+        str += num[i]
       }
     } else {
-      throw "Please enter a valid string";
+      throw "Please enter a valid string"
     }
   }
+
+  let arrOfChar = str.split("<");
+  let splitLength = 0;
+  let formatStr = "";
+
+  arrOfChar.forEach((ar)=>{
+    if (ar.length > 2){
+      splitLength += 1
+    }
+    let excess =ar.slice(2, ar.length);
+    if(excess.length > 0){
+      let first = ar.slice(0, 2);
+      return (formatStr += `${first}[${excess}]`)
+    } else {
+      return (formatStr += ar)
+    }
+  })
+  return [formatStr, splitLength]
 }
-let arrayOfChar = str.split("<");
-let splitLength = 0;
-let forMatStr = "";
 
-arrayOfChar.forEach((ar) => {
-  if (ar.length > 2) {
-    splitLength += 1;
-  }
 
-  let excess = ar.slice(2, ar.length);
 
-  if (excess.length > 0) {
-    let first = ar.slice(0, 2);
-
-    return (forMatStr += `${first}[${excess}]`);
-  } else {
-    return (forMatStr += ar);
-  }
-});
-
-return [forMatStr, splitLength];
 
 module.exports = isolateDuplicates;
